bfc2bacb11d2f04785c0a615c2dbe77d95879a7e
gotimtimm <timothyaaron.go@gmail.com>
2024-10-24 10:28:39 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
 // Placeholder for future interactivity
 console.log("Portfolio website ready!");
+
+// Function to open the overlay
+function openOverlay(overlayId) {
+  document.getElementById(overlayId).style.display = 'flex';
+}
+
+// Function to close the overlay
+function closeOverlay(overlayId) {
+  document.getElementById(overlayId).style.display = 'none';
+}
